851c864a103ce519bc0b8abbd61389e27dc1126f
Assaf <1214assaf@gmail.com>
2024-02-09 12:09:35 +0200

    Add TimerChallenge component and update App and Player components

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,36 @@
+import { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  let timer = useRef(null);
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime !== 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop Challenge" : "Start"}
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
